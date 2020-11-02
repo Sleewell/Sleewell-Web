4edd98bdb763968e21cfbd74bf92b6714f747d9c
root <root@3c6514bb27ef>
2020-11-02 09:43:31 +0000

    First push with everything from server

---
+++ index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+    <link rel="icon" href="./css/icons/sleewell.ico">
+    <link rel="stylesheet" href="css/global.css">
+    <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+    <title> Sleewell </title>
+    <div id="global">
+        <img src="img/logo_sleewell.png" style="width:400px;height:500px;">
+        <h1 id=title>Sleewell</h1>
+    </div>
+    <div class="global-footer"></div>
+    <div id="projet">
+		<h4 style="line-height:100px;text_align:left">
+ 			Retrouver un sommeil d'or
+			<img src="img/bed.png" style="float:left"/>
+		</h4>
+		<h4 style="line-height:100px;text-align:right"><img src="img/reveil.png" style="float:right"/>Votre portable devient un réveil complet</h4>
+		<h4 style="line-height:100px;text-align:left">Déconnexion totale au coucher..<img src="img/deconnexion.png" style="float:left"/></h4>
+		<h4 style="line-height:100px;text-align:right"><img src="img/masque.png" style="float:right"/>Isolez-vous de toutes distractions durant la nuit !</h4>
+    </div>
+    <div id="produits">
+        <h2>Les produits Sleewell</h2>
+        <table width="100%" style="text-align:center">
+            <tr>
+            <td width="40%">
+                <span id="sl_play" class="sl_command"></span>
+                <span id="sl_pause" class="sl_command"></span>
+                <section id="slideshow">
+                    
+                    <a class="play_commands pause" href="#sl_pause" title="Maintain paused">Pause</a>
+                    <a class="play_commands play" href="#sl_play" title="Play the animation">Play</a>
+                    
+                    <div class="container">
+                        <div class="slider">
+                            <figure>
+                                <img src="img/base.png" alt="" width="500" height="375">
+                                <figcaption>Premier prototype</figcaption>
+                            </figure><!--
+                            --><figure>
+                                <img src="img/base1.png" alt="" width="500" height="375">
+                                <figcaption>Premier prototype</figcaption>
+                            </figure><!--
+                            --><figure>
+                                <img src="img/base2.png" alt="" width="500" height="375">
+                                <figcaption>Deuxième prototype</figcaption>
+                            </figure><!--
+                            --><figure>
+                                <img src="img/base3.png" alt="" width="500" height="375">
+                                <figcaption>Deuxième prototype</figcaption>
+                            </figure>
+                        </div>
+                    </div>
+                    <span id="timeline"></span>
+                    
+                </section>
+            </td>
+            <td width="60%"><img src="img/lune.png"><h4>Une base lég&#xE8;re accompagnant votre table de nuit. </h4>
+                                <h4>Activation du protocole d'un simple geste.
+            </h4></td>
+            </tr>
+            <tr>
+                <td width="40%">
+                    <span id="sl_play2" class="sl_command2"></span>
+                    <span id="sl_pause2" class="sl_command2"></span>
+                    <section id="slideshow2">
+                        
+                        <a class="play_commands2 pause2" href="#sl_pause2" title="Maintain paused">Pause</a>
+                        <a class="play_commands2 play2" href="#sl_play2" title="Play the animation">Play</a>
+                        
+                        <div class="container2">
+                            <div class="slider2">
+                                <figure>
+                                    <img src="img/app.png" alt="" width="230" height="500">
+                                    <figcaption>Page principale</figcaption>
+                                </figure><!--
+                                --><figure>
+                                    <img src="img/app1.png" alt="" width="230" height="500">
+                                    <figcaption>Page Statisique</figcaption>
+                                </figure><!--
+                                --><figure>
+                                    <img src="img/app2.png" alt="" width="230" height="500">
+                                    <figcaption>Page paramètres</figcaption>
+                                </figure><!--
+                                --><figure>
+                                    <img src="img/app3.png" alt="" width="230" height="500">
+                                    <figcaption>Page d'alarme</figcaption>
+                                </figure><!--
+                                --><figure>
+                                    <img src="img/app4.png" alt="" width="230" height="500">
+                                    <figcaption>Page choix de son</figcaption>
+                                </figure>
+                            </div>
+                        </div>
+                        <span id="timeline2"></span>
+                        
+                    </section>
+                </td>
+                <td width="60%"><h4>Choisissez vos crit&#xE8;res d'aide d'endormissement..</h4>
+                                    <h4 style="line-height:100px"><img src="img/musique.png" style="float:left">Possibilité de lié votre compte Spotify<h4/>
+                                    <h4 style="line-height:100px">&Eacutetudiez vos données de sommeil et comparez avec des statistiques mondiales. <img src="img/stats.png" style="float:right"></h4></td>
+            </tr>
+        </table>
+    </div>
+    <div class="contacts-header"></div>
+        <div id="contacts">
+        <h1 style="margin-bottom:100px">L'équipe Sleewell vous remercie de votre visite sur le site</h1>
+        <table width="100%" style="text-align:center">
+            <tr>
+                <td width="10%"></td>
+                <td width="10%"></td>
+                <td width="10%"><img src="img/florent.png"></td>
+                <td width="10%"></td>
+                <td width="10%"><img src="img/romane.png"></td>
+                <td width="10%"></td>
+                <td width="10%"><img src="img/gabin.png"></td>
+                <td width="10%"></td>
+                <td width="10%"><img src="img/hugo.png"></td>
+                <td width="10%"></td>
+            </tr>
+            <tr>
+                <td width="10%"></td>
+                <td width="10%"><img src="img/clement.png"></td>
+                <td width="10%"></td>
+                <td width="10%"><img src="img/fabien.png"></td>
+                <td width="10%"></td>
+                <td width="10%"><img src="img/jules.png"></td>
+                <td width="10%"></td>
+                <td width="10%"><img src="img/titouan.png"></td>
+                <td width="10%"></td>
+                <td width="10%"></td>
+            </tr>
+        </table>
+        <h3>Suivez nous sur nos réseaux</h3>
+        <div id="reseaux" style="text-align:center;margin-bottom:50px">
+            <a href="https://twitter.com/sleewell" class="button"><input type="image" class="contact" src="img/twitter.png"></a>
+            <a href="https://www.linkedin.com/company/sleewell" class="button"><input type="image" class="contact" src="img/linkedin.png" style="margin-right:100px;margin-left:100px;"></a>
+            <a href="https://www.instagram.com/sleewell_/" class="button"><input type="image" class="contact" src="img/insta.png"></a>
+        </div>
+        <h3>Si vous souhaitez nous faire parvenir un retour sur l'un des produits ou obtenir plus d'informations, contactez-nous :</h3>
+        <table width="100%">
+            <td width="30%"></td>
+                <td width="40%">
+                    <form action="feedback_form.php" method="post">
+                    <label>Email Address : </label><br/>
+                    <input type="text" name="email_address" size="40" style="margin-bottom:10px;background-color:#00555E;color:#fff"><br/>
+                    <label>Sujet : </label><br/>
+                    <input type="text" name="sujet" size="40" style="margin-bottom:10px;background-color:#00555E;color:#fff"><br/>
+                    <label>Message : </label>
+                    <textarea name="feedback" cols="100" rows="10" style="margin-bottom:10px;background-color:#00555E;color:#fff"></textarea><br/>
+                    <input class="submit-button" type="submit" name="send" value="Send Mail"><br/>
+                </form>
+            </td>
+            <td width="30%"></td>
+        </table>
+    </div>
+</html>
