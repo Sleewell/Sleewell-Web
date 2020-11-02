ade113e051f40a6d394457b1db089e59b0ec9d85
root <root@3c6514bb27ef>
2020-11-02 09:43:13 +0000

    First push with everything from server

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <link rel="icon" href="./sources/icons/sleewell.ico">
+        <link rel="stylesheet" type="text/css" href="./configs/site.css">
+        <title> Sleewelll </title>
+    </head>
+    <body>
+
+    <!-- <div class="linear-gradient"></div> -->
+    <!-- <body style="overflow:hidden" scrolling="no" style="background-image: url(./sources/imgs/site.png);"> -->
+        <!-- <img src="./sources/imgs/site.png" width="100%"></img> -->
+
+
+    </body>
+</html>
