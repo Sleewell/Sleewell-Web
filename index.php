eb9783368014e7abb6df400160d5b4da7aff842b
Manu Pc Clément <manuel.dreano@e-testing.fr>
2020-12-07 18:01:23 +0900

    Merging poc.sleewell.fr to dev.sleewell.fr (Modal login/register + profile page

--- index.php
+++ index.php
@@ -19,11 +19,23 @@ if (isset($_GET['lang']))
         <link rel="icon" href="./css/icons/sleewell.ico">
         <link rel="stylesheet" href="css/global.css">
         <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
-        <link rel="stylesheet" type="text/css" href="css/style.css"/>
         <link rel="stylesheet" href="css/cssanimation.css"> 
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/mdb.min.css" rel="stylesheet"> 
+        <link href="css/font_color_sleewell.css" rel="stylesheet">
+        <link href="css/registerform.css" rel="stylesheet">
+
+        <script type="text/javascript" src="js/jquery.min.js"></script>
+        <script type="text/javascript" src="js/popper.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/mdb.min.js"></script>
+        <script type="text/javascript" src="js/login.js"></script>
+
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="js/anime.min.js"></script>
         <script src="https://kit.fontawesome.com/a54d2cbf95.js"></script>
+
     </head>
     <body>
 
@@ -38,9 +50,10 @@ if (isset($_GET['lang']))
                     <option class="fr" value="<?php echo $_SERVER['PHP_SELF']; ?>?lang=fre" style="background-image:url('img/eng.png');" >French</option>
                     <option class="en" value="<?php echo $_SERVER['PHP_SELF']; ?>?lang=eng" style="background-image:url('img/fra.png');" >English</option>
                 </select>
-                <img src=<?php echo $lang['img_path'];?> style="float:right;width:20px;height:15px;margin-left:5pxmargin-right:30px;margin-top: 18px;"/>
-                <a class="vertical-center" href="contact_us.php"><?php echo $lang['register-redirection'];?></a>
-                <a class="vertical-center" href="contact_us.php"><?php echo $lang['connexion-redirection'];?></a>
+                <img src=<?php echo $lang['img_path'];?> style="float:right;width:20px;height:15px;margin-left:5px;margin-top: 21px;"/>
+                <a href="" class=" vertical-center btn btn-rounded my-3 Amber" data-toggle="modal" data-target="#modalLRForm"><?php echo $lang['connexion-redirection'];?></a>
+                <!-- <a class="vertical-center" href="contact_us.php"><?php echo $lang['register-redirection'];?></a>
+                <a class="vertical-center" href="contact_us.php"><?php echo $lang['connexion-redirection'];?></a> -->
             </div>
         </header>
 
@@ -51,7 +64,7 @@ if (isset($_GET['lang']))
             <td colspan="1" width="40%"></td>
             <td colspan="1" width="20%">
                 <div class="text" data-scrolly-down="blurInBottom, timing-function:cubic-bezier(.17,.67,.83,.67)">
-                    <h1 class="title" style="line-height:250px;text_align:right;margin-top:50px"> <img src="img/logo_sleewell.png" style="float:left;width:200px;height:250px"/>leewell</h1>
+                    <h1 class="title AmeberText" style="line-height:250px;text_align:right;margin-top:50px"> <img src="img/logo_sleewell.png" style="float:left;width:200px;height:250px"/>leewell</h1>
                     <br/><br/><br/>
                 </div>
             </td>
@@ -68,8 +81,8 @@ if (isset($_GET['lang']))
                 <table ><tr>
                     <td colspan="1" width="50%">
                         <div class="text" data-scrolly-down="blurInRight, timing-function:cubic-bezier(.17,.67,.83,.67)">
-                            <h3><?php echo $lang['project-pres1'];?></h3>
-                            <h4><?php echo $lang['project-pres1bis'];?></h4>
+                            <h3 class="AmeberText"><?php echo $lang['project-pres1'];?></h3>
+                            <h4 class="AmeberText"><?php echo $lang['project-pres1bis'];?></h4>
                         </div>
                     </td>
                     <td colspan="1" width="50%">
@@ -90,8 +103,8 @@ if (isset($_GET['lang']))
                     </td>
                     <td colspan="1" width="50%">
                         <div class="text" data-scrolly-down="blurInLeft, timing-function:cubic-bezier(.17,.67,.83,.67)">
-                            <h3><?php echo $lang['project-pres2'];?></h3>
-                            <h4><?php echo $lang['project-pres2bis'];?></h4>
+                            <h3 class="AmeberText"><?php echo $lang['project-pres2'];?></h3>
+                            <h4 class="AmeberText"><?php echo $lang['project-pres2bis'];?></h4>
                         </div>
                     </td>
                 </tr></table>
@@ -102,8 +115,8 @@ if (isset($_GET['lang']))
                 <table ><tr>
                     <td colspan="1" width="50%">
                         <div class="text" data-scrolly-down="blurInRight, timing-function:cubic-bezier(.17,.67,.83,.67)">
-                            <h3><?php echo $lang['project-pres3'];?></h3>
-                            <h4><?php echo $lang['project-pres3bis'];?></h4>
+                            <h3 class="AmeberText"><?php echo $lang['project-pres3'];?></h3>
+                            <h4 class="AmeberText"><?php echo $lang['project-pres3bis'];?></h4>
                         </div>
                     </td>
                     <td colspan="1" width="50%">
@@ -124,8 +137,8 @@ if (isset($_GET['lang']))
                     </td>
                     <td colspan="1" width="50%">
                         <div class="text" data-scrolly-down="blurInLeft, timing-function:cubic-bezier(.17,.67,.83,.67)">
-                            <h3><?php echo $lang['project-pres4'];?></h3>
-                            <h4><?php echo $lang['project-pres4bis'];?></h4>
+                            <h3 class="AmeberText"><?php echo $lang['project-pres4'];?></h3>
+                            <h4 class="AmeberText"><?php echo $lang['project-pres4bis'];?></h4>
                         </div>
                     </td>
                 </tr></table>
@@ -137,13 +150,13 @@ if (isset($_GET['lang']))
         <!--################################-->
         <div id="products">
             <table style="width:100%"><tr>
-                </br><h1><?php echo $lang['Products-pres'];?></h1>
+                </br><h1 class="AmeberText"><?php echo $lang['Products-pres'];?></h1>
             </tr><tr>
                 <td colspan="1" width="50%">
-                    <h2>Base</h2>
+                    <h2 class="AmeberText">Base</h2>
                 </td>
                 <td colspan="1" width="50%">
-                    <h2>Application</h2>
+                    <h2 class="AmeberText">Application</h2>
                 </td>
             </tr><tr>
                 <td colspan="1" width="50%">
@@ -166,6 +179,108 @@ if (isset($_GET['lang']))
         </script>
 
         <!--################################-->
+        <!--      MODAL LOGIN REGISTER      -->
+        <!--################################-->
+
+        <div class="modal fade " id="modalLRForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog GlobalBackground cascading-modal" role="document">
+                <div class="modal-content">
+                    <div class="modal-c-tabs">
+                        <ul class="nav nav-tabs md-tabs tabs-2 white darken-3" role="tablist">
+                            <li class="nav-item">
+                                <a class="notActive nav-link active" data-toggle="tab" href="#panel1" role="tab"><i class="fas fa-user mr-1"></i>Se connecter</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="notActive nav-link" data-toggle="tab" href="#panel2" role="tab"><i class="fas fa-user-plus mr-1"></i>S'enregistrer</a>
+                            </li>
+                        </ul>
+                        <div class="tab-content">
+                            <div class="tab-pane fade in show active" id="panel1" role="tabpanel">
+                                <form id="loginForm" action="profile.php" method="post">
+                                <div class="modal-body mb-1">
+                                    <div class="md-form form-sm mb-5">
+                                        <i class="fas fa-user prefix"></i>
+                                        <input type="text" onkeyup="updateLoginInput()" name="id" id="modalLRInput10" class="form-control form-control-sm validate">
+                                        <label data-success="Super !" for="modalLRInput10">Votre identifiant</label>
+                                    </div>
+                                    <div class="md-form form-sm mb-4">
+                                        <i class="fas fa-lock prefix"></i>
+                                        <input type="password" onkeyup="updateLoginInput()" id="modalLRInput11" class="form-control form-control-sm">
+                                        <label name="password" for="modalLRInput11">Votre superbe mot de passe</label>
+                                    </div>
+                                    <div class="text-center mt-2">
+                                        <input type="button" id="LoginButton" class="btn Mango" value="Se connecter" disabled></input>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <div class="options text-center text-md-right mt-1">
+                                        <p>Forgot <a href="#" class="blue-text">Password?</a></p>
+                                    </div>
+                                    <button type="button" class="btn waves-effect ml-auto closeButtonMango" data-dismiss="modal">Fermer</button>
+                                </div>
+                                </form>
+                            </div>
+                            <div class="tab-pane fade" id="panel2" role="tabpanel">
+                                <form id="registerForm" action="profile.php" method="post">
+                                <div class="modal-body">
+                                    <div class="form-row">
+                                        <div class="col">
+                                            <div class="md-form">
+                                                <input type="text" id="registerLastName" class="form-control">
+                                                <label for="registerLastName">Votre nom</label>
+                                            </div>
+                                        </div>
+                                        <div class="col">
+                                            <div class="md-form">
+                                                <input type="text" id="registerFirstName" class="form-control">
+                                                <label for="registerFirstName">Votre prénom</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="md-form mt-0">
+                                        <input type="text" id="registerUsername" class="form-control">
+                                        <label for="registerUsername">Votre identifiant</label>
+                                    </div>
+                                    <div class="md-form mt-0">
+                                        <input onchange="checkRegisterMail()" type="email" id="registerEmail" class="form-control validate">
+                                        <label data-error="Invalide" data-success="Super" for="registerEmail">Votre email</label>
+                                        <!-- <small style="display: none;" id="registerPasswordHelpBlock" class="form-text text-muted mb-4">
+                                            Votre email ne comporte pas @ ! -->
+                                        </small>
+                                    </div>
+                                    <div class="md-form">
+                                        <input type="password" onkeyup="checkRegisterPassword()" id="registerPassword" class="form-control" aria-describedby="registerPasswordPasswordHelpBlock">
+                                        <label for="registerPassword">Votre mot de passe</label>
+                                        <small id="registerPasswordHelpBlock" class="form-text text-muted mb-4">
+                                            Votre superbe mot de passe doit au moins contenir 8 caractères et 1 chiffre
+                                        </small>
+                                    </div>
+                                    <div class="md-form">
+                                        <input type="password" onkeyup="checkRegisterPassword()" id="registerPasswordCheck" class="form-control" aria-describedby="registerPasswordCheckPasswordHelpBlock">
+                                        <label for="registerPasswordCheck">Votre mot de passe (le même)</label>
+                                    </div>
+                                    <div id="message">
+                                    <h5>Votre mot de passe ne contient pas les éléments suivants:</h5>
+                                        <p id="number" class="invalid">Un <b>chiffre</b></p>
+                                        <p id="length" class="invalid">Au moins <b>8 caractères</b></p>
+                                        <p id="same" class="invalid">Ne sont pas <b>identiques</b></p>
+                                    </div>
+                                    <div class="text-center form-sm mt-2">
+                                        <input type="button" id="RegisterButton" class="btn Mango" value="S'enregistrer" disabled></input>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn  waves-effect ml-auto closeButtonMango" data-dismiss="modal">Fermer</button>
+                                </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!--################################-->
         <!--          PIED DE PAGE          -->
         <!--################################-->
         <footer>
@@ -174,8 +289,8 @@ if (isset($_GET['lang']))
                     <img src="img/logo_sleewell.png" style="float:left;width:120px;height:150px;margin-left:100px"/>
                 </td>
                 <td colspan="1" width="40%">
-                    <h3>SUPPORT</h3>
-                    <a href="contact_us.php"><h4><?php echo $lang['contact-redirection'];?></h4></a>
+                    <h3 class="AmeberText">SUPPORT</h3>
+                    <a href="contact_us.php"><h4 class="AmeberText"><?php echo $lang['contact-redirection'];?></h4></a>
                 </td>
             </tr></table>
         </footer>
