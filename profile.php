382d0c04325d847fe3ce1efe9a20419ec5cef7df
Manu Pc Clément <manuel.dreano@e-testing.fr>
2020-12-07 18:25:04 +0900

    Add session php in profile.php

--- profile.php
+++ profile.php
@@ -1,3 +1,16 @@
+<?php
+session_start();
+if ( ! isset($_SESSION['lang'])) $_SESSION['lang'] = 'eng';
+require ("lang/{$_SESSION['lang']}.php");
+
+if (isset($_GET['lang']))
+{
+    $_SESSION['lang'] = $_GET['lang'];
+    header("Location: {$_SERVER['PHP_SELF']}");
+    exit;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
