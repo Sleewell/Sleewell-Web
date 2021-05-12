256dfa4de821d9c1dc13ce916a636554b37a6c0d
Clément <clement.dumaine@epitech.eu>
2021-05-12 17:42:17 +0900

    -1 day

--- js/profileGraph.js
+++ js/profileGraph.js
@@ -37,7 +37,7 @@ function showGraph(nb)
         {
             console.log(token);
             if (nb == 1) {
-                const today = moment().format('YYYYMMDD');
+                const today = moment().format('YYYYMMDD').add(-1, "days");
                 var settings = {
                     "url": "https://api.sleewell.fr/stats/night/" + today,
                     "method": "GET",
